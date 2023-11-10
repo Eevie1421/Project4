975a401411597deeb1842f3c70d89571f621bd78
salim <salimjday@outlook.com>
2023-11-09 21:36:13 -0500

    update

--- Player.java
+++ Player.java
@@ -1,13 +1,7 @@
-
 import java.util.Hashtable;
+import java.util.Scanner;
 
-/* Player v 0.1
- * Player's job is to keep track of health and status.
- * Player will also have different attack/ability options in future versions
- * Player will have and allow interaction with items.
- */
 public class Player {
-    //status 0 is default, 1 is unconscious, 2 is dead. Room for more types later.
     private int status;
     private int maxHealth;
     private int health;
@@ -17,7 +11,7 @@ public class Player {
     private int playerAc;
     private int attackMod;
 
-    public Player(String name, int playerClass){
+    public Player(String name, int playerClass) {
         status = 0;
         maxHealth = 50;
         health = 50;
@@ -28,100 +22,115 @@ public class Player {
         pickItem(new Item(1));
     }
 
-    public Player(){
-        this("John", 1);
+    public void setClassStats() {
+        if (playerClass == 1) {
+            playerAc = 15;
+            attackMod = 3;
+        } else {
+            playerAc = 12;
+            attackMod = 1;
+        }
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAc(){
+    public int getAc() {
         return playerAc;
+    }
+
+    public void pickItem(Item item) {
+        backpack.put(item.getType(), item);
+    }
+
+    public boolean useItem(String key) {
+        Item temp = backpack.get(key);
+        if (temp != null && !temp.outOfCharges()) {
+            int val = temp.use();
+            if (temp.outOfCharges()) {
+                backpack.remove(key);
+            }
+            if ("Health pot".equals(key)) {
+                updateHealth(val);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int attack1(int enemyAC) {
+        int attackRoll = Dice.rollD20(1, attackMod);
+        int damageRoll = 0;
+
+        if (attackRoll >= enemyAC) {
+            damageRoll = sword(); // Or mace() based on player class
+        }
+
+        return damageRoll;
+    }
+
+    private int sword() {
+        return Dice.rollD10(1, attackMod);
     }
 
     public int checkHealth() {
         return health;
     }
 
-    public int checkStatus() {
-        return status;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public void setMaxHealth(int maxHealth) {
-        this.maxHealth = maxHealth;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setClassStats(){
-        if(playerClass == 1){
-            playerAc = 15;
-            attackMod = 3;
-        }
-        else{
-            playerAc = 12;
-            attackMod = 1;
-        }
-    }
-
-    public void updateStatus(int status){
+    public void updateStatus(int status) {
         this.status = status;
     }
 
-    public void updateHealth(int damage){
-        health = health - damage;
-        if(health <= 0){
+    public boolean isAlive() {
+        return status == 0;
+    }
+
+    public void updateHealth(int damage) {
+        health -= damage;
+        if (health <= 0) {
             health = 0;
             updateStatus(1);
-        }
-        else if(health > maxHealth){
+        } else if (health > maxHealth) {
             health = maxHealth;
         }
-        if(status > 0 && health > 0){
+        if (status > 0 && health > 0) {
             status = 0;
         }
     }
-    public void pickItem(Item item){
-        backpack.put(item.getType(), item);
-    }
 
-    public boolean useItem(String key){
-        Item temp = backpack.get(key);
-        if(temp.outOfCharges()){
-            return false;
-        }
-        int val = temp.use();
-        if(temp.outOfCharges()){
-            backpack.remove(key);
-        }
-        if(key.equals("Health pot")){
-            updateHealth(val);
-        }
-        return true;
-    }
+    public void playTurn(Room currentRoom) {
+        Scanner scanner = new Scanner(System.in);
 
-    public int attack1(int ac){
-        int attackRoll = Dice.rollD20(1, attackMod);
-        int damageRoll = 0;
-        if(playerClass == 1 && attackRoll >= ac){
-            damageRoll = sword();
-        }
-        else if(playerClass == 2 && attackRoll >= ac){
-            damageRoll = mace();
-        }
-        return damageRoll;
-    }
-    private int sword(){
-        return Dice.rollD10(1, attackMod);
-    }
+        System.out.println("It's your turn to act!");
 
-    private int mace(){
-        return Dice.rollD6(1, attackMod);
+        // Print information about the current room
+        System.out.println("Current Room: " + currentRoom.getDescription());
+
+        // Check if there is an enemy in the room
+        if (currentRoom.getEnemy() != null) {
+            // Handle combat
+            System.out.println("An enemy is in the room! Do you want to attack? (yes/no)");
+            String userInput = scanner.nextLine();
+
+            if ("yes".equalsIgnoreCase(userInput)) {
+                int damage = attack1(currentRoom.getEnemy().getEnemyAc());
+                currentRoom.getEnemy().updateHealth(damage);
+                System.out.println("You hit the enemy for " + damage + " damage!");
+            } else {
+                System.out.println("You decided not to attack.");
+            }
+
+            // ... Handle enemy's attack and check for player defeat ...
+        }
+
+        // Handle other room interactions and move to the next room based on user input
+        // For simplicity, let's assume there's always a next room
+        System.out.println("Do you want to proceed to the next room? (yes/no)");
+        String userInput = scanner.nextLine();
+
+        if ("yes".equalsIgnoreCase(userInput)) {
+            System.out.println("You proceed to the next room.");
+            // Move to the next room (adjust this logic based on your actual implementation)
+        } else {
+            System.out.println("You decided not to proceed to the next room.");
+            // End the game or take other actions based on your game design
+        }
     }
 }
