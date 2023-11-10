2f1702f4b09fc11c98f45fe367405c79a5156638
salim <salimjday@outlook.com>
2023-11-09 21:36:13 -0500

    update

---
+++ Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        // Initialize player
+        Player player = new Player("Player1", 1);
+
+        // Initialize rooms
+        Room combatRoom1 = new Room("Combat Room 1", new Zombie(), null, null);
+        Room combatRoom2 = new Room("Combat Room 2", new Zombie(), null, null);
+        Room healRoom1 = new Room("Heal Room 1", null, new Item(1), null);
+        Room combatRoom3 = new Room("Combat Room 3", new Zombie(), null, null);
+        Room itemRoom2 = new Room("Item Room 2", null, new Item(2), null);
+        Room combatRoom4 = new Room("Combat Room 4", new Zombie(), null, null);
+        Room healRoom2 = new Room("Heal Room 2", null, new Item(3), null);
+        Room objective2 = new Room("Objective 2", null, null, null);
+        Room combatRoom5 = new Room("Combat Room 5 (Boss Fight)", new Boss(), null, null);
+
+        // Set up the room
+        combatRoom1.setNextRoom(combatRoom2);
+        combatRoom2.setNextRoom(healRoom1);
+        healRoom1.setNextRoom(combatRoom3);
+        combatRoom3.setNextRoom(itemRoom2);
+        itemRoom2.setNextRoom(combatRoom4);
+        combatRoom4.setNextRoom(healRoom2);
+        healRoom2.setNextRoom(objective2);
+        objective2.setNextRoom(combatRoom5);
+
+        // Set the starting room
+        Room startingRoom = combatRoom1;
+
+        // Initialize game logic
+        GameLogic gameLogic = new GameLogic(player, startingRoom);
+
+        // Start the game
+        gameLogic.play();
+    }
+}
