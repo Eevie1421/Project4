7235a44746ee2063f6ffb20ce925a96e0b5219e4
RealJonMurphOfficial <jonathan.p.murphy49@gmail.com>
2023-11-13 17:04:23 -0500

    end panel with rude message

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         EndPanel pane = new EndPanel();
-        frame.setTitle("Hiker Register");
+        frame.setTitle("Rude panel");
         frame.getContentPane().add(pane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
