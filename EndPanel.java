9785ae795a4fb77f7bee112f9d027de9f1c32305
RealJonMurphOfficial <jonathan.p.murphy49@gmail.com>
2023-11-13 17:00:06 -0500

    end panel with rude message

---
+++ EndPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class EndPanel extends JPanel {
+    private JTextArea message;
+
+    public EndPanel() {
+        setPreferredSize(new Dimension(1600, 1200));
+        message = new JTextArea("You won! Now go away.");
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 4;
+        c.gridy = 1;
+        c.gridheight = 8;
+        c.gridwidth = 4;
+        add(message, c);
+    }
+}
